0e18583cc1e76387c2a1f41c473409a358548049
Nicolas Grimaud <ketchu13@hotmail.com>
2017-03-24 00:02:36 +0100

    Edit Credits, remove some languages

--- cube/swiss/include/filelist.h
+++ cube/swiss/include/filelist.h
@@ -1,11 +1,10 @@
 /****************************************************************************
+ * From Snes9x Nintendo Wii/Gamecube Port
  *
- * Ketchu13 2017
+ * Tantric 2009-2010
  *
- * filelist.h
- *
- * Contains a list of all of i18N the files stored in lang/
  ***************************************************************************/
+ // Adapted to the Greatest SwissGC of emu_kidid by ketchu13 2017
 
 #ifndef _FILELIST_H_
 #define _FILELIST_H_
@@ -13,8 +12,7 @@
 #include <gccore.h>
 
 // Languages
-extern const uint8_t		jp_lang[];
-extern const uint32_t	jp_lang_size;
+
 extern const uint8_t		en_lang[];
 extern const uint32_t	en_lang_size;
 extern const uint8_t		de_lang[];
@@ -28,7 +26,7 @@ extern const uint32_t	it_lang_size;
 extern const uint8_t		nl_lang[];
 extern const uint32_t	nl_lang_size;
 extern const uint8_t		zh_lang[];
-extern const uint32_t	zh_lang_size;
+/*extern const uint32_t	zh_lang_size;
 extern const uint8_t		ko_lang[];
 extern const uint32_t	ko_lang_size;
 extern const uint8_t		pt_lang[];
@@ -39,5 +37,7 @@ extern const uint8_t		ca_lang[];
 extern const uint32_t	ca_lang_size;
 extern const uint8_t		tr_lang[];
 extern const uint32_t	tr_lang_size;
+extern const uint8_t		jp_lang[];
+extern const uint32_t	jp_lang_size;*/
 
 #endif
