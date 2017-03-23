28ce71bcd760006ef27d94874bf26683d824a600
Nicolas Grimaud <ketchu13@hotmail.com>
2017-03-24 00:03:37 +0100

    Edit Credits

--- cube/swiss/include/gettext.h
+++ cube/swiss/include/gettext.h
@@ -1,3 +1,11 @@
+/****************************************************************************
+ * From Snes9x Nintendo Wii/Gamecube Port
+ *
+ * Tantric 2009-2010
+ *
+ ***************************************************************************/
+// Adapted to the Greatest SwissGC of emu_kidid by ketchu13 2017
+
 #ifndef _GETTEXT_H_
 #define _GETTEXT_H_
 
